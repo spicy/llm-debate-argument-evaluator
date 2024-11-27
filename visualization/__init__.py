2c6b1bbb4b7cd963237dad594a34cff9f60b3c8b
LouisOporto <louisoporto042@csu.fullerton.edu>
2024-11-26 18:49:04 -0800

    Remove older version of renderer

--- visualization/__init__.py
+++ visualization/__init__.py
@@ -1,18 +1,12 @@
-from .node_expansion_handler import NodeExpansionHandler
 from .node_score_display import NodeScoreDisplay
 from .observer import Observer, DebateTreeSubject
-from .tree_renderer import TreeRenderer
 from .visualization_injector import VisualizationInjector
 from .modified_renderer import ModifiedRenderer
 
 __all__ = [
-    "NodeExpansionHandler",
     "NodeScoreDisplay",
     "Observer",
     "DebateTreeSubject",
-    "TreeRenderer",
     "VisualizationInjector",
     "ModifiedRenderer",
 ]
-
-# Not used NodeExpansionHandler,
--- visualization/visualization_injector.py
+++ visualization/visualization_injector.py
@@ -1,9 +1,7 @@
 from utils.logger import logger
 
-from .node_expansion_handler import NodeExpansionHandler
 from .node_score_display import NodeScoreDisplay
 from .observer import DebateTreeSubject
-from .tree_renderer import TreeRenderer
 from .modified_renderer import ModifiedRenderer
 
 
@@ -13,14 +11,10 @@ class VisualizationInjector:
         logger.info("Injecting visualization services")
 
         debate_tree_subject = DebateTreeSubject()
-        tree_renderer = TreeRenderer(debate_tree_subject)
-        node_expansion_handler = NodeExpansionHandler(debate_tree_subject)
         node_score_display = NodeScoreDisplay(debate_tree_subject)
         modified_renderer = ModifiedRenderer(debate_tree_subject)
 
         registry.register("debate_tree_subject", debate_tree_subject)
-        registry.register("tree_renderer", tree_renderer)
-        registry.register("node_expansion_handler", node_expansion_handler)
         registry.register("node_score_display", node_score_display)
         registry.register("modified_renderer", modified_renderer)
 
