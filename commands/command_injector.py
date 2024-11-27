b46cb29a42744792ba4818d369bb96cbe4f7b408
LouisOporto <louisoporto042@csu.fullerton.edu>
2024-11-26 20:52:40 -0800

    Implement average scoring after evaluation and note that expand, generation needs to be implemented

--- commands/command_injector.py
+++ commands/command_injector.py
@@ -17,21 +17,21 @@ class CommandInjector:
 
         registry.register(
             "expand_node_command",
-            ExpandNodeCommand(argument_generation_service, evaluation_service, priority_queue_service),
+            ExpandNodeCommand(argument_generation_service, evaluation_service, priority_queue_service), # Need to add score_aggregator
         )
         registry.register(
             "submit_argument_command",
-            SubmitArgumentCommand(evaluation_service, priority_queue_service),
+            SubmitArgumentCommand(evaluation_service, priority_queue_service, score_aggregator_service),
         )
         registry.register(
             "generate_arguments_command",
             GenerateArgumentsCommand(
-                argument_generation_service, evaluation_service, priority_queue_service
+                argument_generation_service, evaluation_service, priority_queue_service # Need to add score_aggregator
             ),
         )
         registry.register(
             "evaluate_arguments_command",
-            EvaluateArgumentsCommand(evaluation_service, score_aggregator_service),
+            EvaluateArgumentsCommand(evaluation_service, score_aggregator_service), # Not too important to have
         )
 
         logger.info("Commands injected successfully")
