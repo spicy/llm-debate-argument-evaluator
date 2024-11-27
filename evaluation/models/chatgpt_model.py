4b5f0554783ecee7c29017cf28ec3ee906830c8c
LouisOporto <louisoporto042@csu.fullerton.edu>
2024-11-26 20:49:10 -0800

    Add system message for evaluation to ensure decmial values as output from CHATGPT

--- evaluation/models/chatgpt_model.py
+++ evaluation/models/chatgpt_model.py
@@ -11,7 +11,7 @@ class ChatGPTModel(BaseLLMModel):
         self.api_client = ChatGPTAPIClient()
         self.evaluation_prompts: Dict[str, Callable[[str], str]] = {
             "coherence": lambda arg: (
-                f"Evaluate the coherence of the following argument on a scale of 0 to 100: '{arg}'"
+                f"Evaluate the coherence of the following argument on a decimal scale of 0 to 1.00: '{arg}'"
             ),
             "persuasion": lambda arg: (
                 f"Evaluate the persuasiveness of the following argument on a scale of 0 to 100: '{arg}'"
@@ -20,9 +20,15 @@ class ChatGPTModel(BaseLLMModel):
                 f"Evaluate the cultural acceptance of the following argument on a scale of 0 to 100: '{arg}'"
             ),
             "factual_accuracy": lambda arg: (
-                f"Evaluate the factual accuracy of the following argument on a scale of 0 to 100: '{arg}'"
+                f"Evaluate the factual accuracy of the following argument giving on a scale of 0 to 100: '{arg}'"
             ),
         }
+        self.eval_system_message = (
+                f"You are an AI assistant tasked with evaluating the given argument in terms of the criteria given. "
+                f"You must be an unbiased judge to the argument provided. "
+                f"The evaluation should be only a value between 0 to 1. "
+                f"Explicitly print out only the value as a float and nothing else. "
+            )
 
     @log_execution_time
     async def _evaluate(self, evaluation_type: str, argument: str) -> float:
@@ -31,7 +37,7 @@ class ChatGPTModel(BaseLLMModel):
             raise ValueError(f"Invalid evaluation type: {evaluation_type}")
         prompt = self.evaluation_prompts[evaluation_type](argument)
         logger.debug(f"Evaluating {evaluation_type} for ChatGPT model")
-        return await self.api_client.evaluate(prompt)
+        return await self.api_client.evaluate(self.eval_system_message, prompt)
 
     async def evaluate_coherence(self, argument: str) -> float:
         return await self._evaluate("coherence", argument)
